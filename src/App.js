1986b9b7ccd8a2d1341aebe2d3fc9e70d35109e8
Jan Piotrowski <janfpiotrowski@gmail.com>
2021-10-22 14:12:12 +0200

    Config Chakra and eslint

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme
+} from '@chakra-ui/react'; import './App.css';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit
-                    {' '}
-                    <code>src/App.js</code>
-                    {' '}
-                    and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+const App = () => (
+    <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <VStack spacing={8}>
+                    <Text>
+                        Edit
+                        {' '}
+                        <Code fontSize="xl">src/App.js</Code>
+                        {' '}
+                        and save to reload.
+                    </Text>
+                    <Link
+                        color="teal.500"
+                        href="https://chakra-ui.com"
+                        fontSize="2xl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn Chakra
+                    </Link>
+                </VStack>
+            </Grid>
+        </Box>
+    </ChakraProvider>
+);
 
 export default App;
