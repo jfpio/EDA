27f540668f24f84c0de064b66e222a1a4bc21421
Jan Piotrowski <janfpiotrowski@gmail.com>
2021-11-19 16:55:39 +0100

    Add ChartConfigDrawer

---
+++ src/containers/ChartConfigDrawer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Stack, Tag } from '@chakra-ui/react';
+
+export const ChartConfigDrawer = () => (
+    <Stack w="full" h="full" p={2}>
+        <Tag size="lg">X</Tag>
+        <Tag size="lg">Y</Tag>
+    </Stack>
+);
+
--- src/containers/MainView/MainView.js
+++ src/containers/MainView/MainView.js
@@ -4,21 +4,23 @@ import { Header } from '../Header';
 import { DataSourceDrawer } from '../DataSourceDrawer';
 import { WorkingSpace } from '../WorkingSpace';
 import { Footer } from '../Footer';
+import { ChartConfigDrawer } from '../ChartConfigDrawer';
 
 export const MainView = () => (
     <Flex h="100vh" p={6}>
         <Grid
             gridTemplateRows="100px 1fr 50px"
-            templateColumns="200px 1fr"
+            templateColumns="200px 200px 1fr"
             gridGap={1}
             width="100%"
         >
-            <GridItem gridColumn="1 / 3">
+            <GridItem gridColumn="1 / 4">
                 <Header />
             </GridItem>
             <DataSourceDrawer />
+            <ChartConfigDrawer />
             <WorkingSpace />
-            <GridItem gridColumn="1 / 3">
+            <GridItem gridColumn="1 / 4">
                 <Footer />
             </GridItem>
         </Grid>
