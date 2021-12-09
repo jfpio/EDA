0ce92b0700478fd5a947a03f0405273f98f5c184
Jan Piotrowski <janfpiotrowski@gmail.com>
2021-12-09 13:30:37 +0100

    Fix problem with DnD with changing Popover

--- src/containers/Attributes/components/Attribute.js
+++ src/containers/Attributes/components/Attribute.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton } from '@chakra-ui/react';
-import { AtSignIcon, HamburgerIcon, TimeIcon } from '@chakra-ui/icons';
 import { DnDItemTypes } from '../../../DnD';
 import { DraggableTag } from '../../../components/DnD/DraggableTag';
 import { FieldPopover } from './FieldPopover';
@@ -11,42 +9,20 @@ export const Attribute = ({
     name,
     fieldType,
     onChange
-}) => {
-    const getIcon = (fieldType) => {
-        switch (fieldType) {
-            case VEGA_DATA_TYPES.TEMPORAL:
-                return <TimeIcon />;
-            case VEGA_DATA_TYPES.QUANTITATIVE:
-                return <HamburgerIcon />;
-            case VEGA_DATA_TYPES.NOMINAL:
-            default:
-                return <AtSignIcon />;
-        }
-    };
-
-    return (
-        <DraggableTag
-            tagId={name}
-            itemTypeKey={DnDItemTypes.ATTRIBUTE}
-            icon={(
-                <FieldPopover
-                    value={fieldType}
-                    button={(
-                        <IconButton
-                            aria-label="Choose type of field"
-                            icon={getIcon(fieldType)}
-                            size="xs"
-                            background="white"
-                        />
-                    )}
-                    onChange={onChange}
-                />
-            )}
-        >
-            {name}
-        </DraggableTag>
-    );
-};
+}) => (
+    <DraggableTag
+        tagId={name}
+        itemTypeKey={DnDItemTypes.ATTRIBUTE}
+        icon={(
+            <FieldPopover
+                value={fieldType}
+                onChange={onChange}
+            />
+        )}
+    >
+        {name}
+    </DraggableTag>
+);
 
 Attribute.propTypes = {
     name: PropTypes.string.isRequired,
--- src/containers/Attributes/components/FieldPopover.js
+++ src/containers/Attributes/components/FieldPopover.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+    IconButton,
     Popover,
     PopoverArrow, PopoverBody,
     PopoverCloseButton,
@@ -8,35 +9,62 @@ import {
     PopoverHeader,
     PopoverTrigger, Radio, RadioGroup, VStack
 } from '@chakra-ui/react';
+import { AtSignIcon, HamburgerIcon, TimeIcon } from '@chakra-ui/icons';
 import { VEGA_DATA_TYPES } from '../../../redux/chartConfig/const';
 
-export const FieldPopover = ({ button, onChange, value }) => (
-    <Popover
-        variant="responsive"
-    >
-        <PopoverTrigger>
-            {button}
-        </PopoverTrigger>
-        <PopoverContent>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverHeader>Choose Vega data type</PopoverHeader>
-            <PopoverBody>
-                <RadioGroup onChange={onChange} value={value}>
-                    <VStack align="start">
-                        <Radio value={VEGA_DATA_TYPES.QUANTITATIVE}>Quantitative</Radio>
-                        <Radio value={VEGA_DATA_TYPES.ORDINAL}>Ordinal</Radio>
-                        <Radio value={VEGA_DATA_TYPES.NOMINAL}>Nominal</Radio>
-                        <Radio value={VEGA_DATA_TYPES.TEMPORAL}>Temporal</Radio>
-                    </VStack>
-                </RadioGroup>
-            </PopoverBody>
-        </PopoverContent>
-    </Popover>
-);
+export const FieldPopover = ({ value, onChange }) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const open = () => setIsOpen(!isOpen);
+    const close = () => setIsOpen(false);
+
+    const getIcon = (type) => {
+        switch (type) {
+            case VEGA_DATA_TYPES.TEMPORAL:
+                return <TimeIcon />;
+            case VEGA_DATA_TYPES.QUANTITATIVE:
+                return <HamburgerIcon />;
+            case VEGA_DATA_TYPES.NOMINAL:
+            default:
+                return <AtSignIcon />;
+        }
+    };
+    return (
+        <Popover
+            variant="responsive"
+            isOpen={isOpen}
+            onClose={close}
+            autoFocus={false}
+            returnFocusOnClose={false}
+        >
+            <PopoverTrigger>
+                <IconButton
+                    aria-label="Choose type of field"
+                    icon={getIcon(value)}
+                    size="xs"
+                    background="white"
+                    onClick={() => open()}
+                />
+            </PopoverTrigger>
+            <PopoverContent style={!isOpen && ({ display: 'none' })}>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Choose Vega data type</PopoverHeader>
+                <PopoverBody>
+                    <RadioGroup onChange={onChange} value={value}>
+                        <VStack align="start">
+                            <Radio value={VEGA_DATA_TYPES.QUANTITATIVE}>Quantitative</Radio>
+                            <Radio value={VEGA_DATA_TYPES.ORDINAL}>Ordinal</Radio>
+                            <Radio value={VEGA_DATA_TYPES.NOMINAL}>Nominal</Radio>
+                            <Radio value={VEGA_DATA_TYPES.TEMPORAL}>Temporal</Radio>
+                        </VStack>
+                    </RadioGroup>
+                </PopoverBody>
+            </PopoverContent>
+        </Popover>
+    );
+};
 
 FieldPopover.propTypes = {
-    button: PropTypes.element.isRequired,
     value: PropTypes.oneOf(Object.values(VEGA_DATA_TYPES)).isRequired,
     onChange: PropTypes.func.isRequired
 };
