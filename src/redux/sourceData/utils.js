833de2ffb08e39a94555e5bd457dbf4d7a75a372
Jan Piotrowski <janfpiotrowski@gmail.com>
2021-12-11 19:23:53 +0100

    Add describe and it into util function tests

--- src/redux/sourceData/utils.js
+++ src/redux/sourceData/utils.js
@@ -1,4 +1,5 @@
 import { isNil } from 'ramda';
+import { VEGA_DATA_TYPES } from '../vegaEncoding/const';
 
 export const fetchColumnsAndRowsFromJSON = (json) => {
     if (isNil(json) || json.length === 0 || json[0].length === 0) {
@@ -9,4 +10,14 @@ export const fetchColumnsAndRowsFromJSON = (json) => {
         columns: Object.keys(json[0]),
         rows: json
     };
+};
+
+export const getRecommendedDatatype = (value) => {
+    if (Number(value)) {
+        return VEGA_DATA_TYPES.QUANTITATIVE;
+    }
+    if (!Number.isNaN(Date.parse(value))) {
+        return VEGA_DATA_TYPES.TEMPORAL;
+    }
+    return VEGA_DATA_TYPES.NOMINAL;
 };
